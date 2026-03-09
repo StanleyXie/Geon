a7419fb3a4d1640e6875edaf9e49784dc1879c9d
StanleyXie <stanley.xie@outlook.com>
2026-03-09 09:53:34 +0100

    chore: bump internal version to 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ process.on("unhandledRejection", (reason) => {
 });
 
 if (process.argv.includes("--version") || process.argv.includes("-v")) {
-  console.error("Geon v0.1.1");
+  console.error("Geon v0.1.4");
   process.exit(0);
 }
 
